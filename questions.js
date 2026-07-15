f37347f923c559ecd2fd7c9a0f19121d5bbae98a
v87s2vp9vp-ctrl <v87s2vp9vp@privaterelay.appleid.com>
2026-07-15 08:54:30 +0200

    questions.js aktualisieren

--- questions.js
+++ questions.js
@@ -1,1 +1,27 @@
-.
+const questions = [
+
+{
+question:"Hatte ich heute einen schönen Moment?"
+},
+
+{
+question:"Habe ich heute etwas für meine Ziele getan?"
+},
+
+{
+question:"War ich heute auf etwas stolz?"
+},
+
+{
+question:"Hat mich heute etwas aufgehalten?"
+},
+
+{
+question:"Bin ich heute dankbar?"
+},
+
+{
+question:"Konnte ich heute mit Stolz in den Spiegel schauen?"
+}
+
+];
